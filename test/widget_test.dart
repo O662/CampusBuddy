41647668c6b4e06d297c556c2f2d513611f05793
Malcolm <buildingwithkieth@outlook.com>
2026-05-16 15:06:13 -0700

    Reset

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:campus_buddy/main.dart';
-
-void main() {
-  testWidgets('App builds smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const CampusBuddyApp());
-    expect(find.byType(CampusBuddyApp), findsOneWidget);
-  });
-}
